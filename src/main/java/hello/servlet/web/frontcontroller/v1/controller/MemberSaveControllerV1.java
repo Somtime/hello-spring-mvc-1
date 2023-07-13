10e6d999570bac17c1c05629a6e577b43f76e8ae
Somtime <rghee0821@gmail.com>
2023-07-13 13:40:37 +0900

    섹션 4. MVC 프레임워크 만들기
    4-1) 프론트 컨트롤러 패턴 소개
    4-2) 프론트 컨트롤러 도입 -v1
    4-3) View 분리 -v2
    4-4) Model 추가 -v3
    4-5) 단순하고 실용적인 컨트롤러 - v4
    4-6) 유연한 컨트롤러1 - v5
    4-7) 유연한 컨트롤러2 - v5

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Enumeration<String> parameters = request.getParameterNames();
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(request, response);
+    }
+}
